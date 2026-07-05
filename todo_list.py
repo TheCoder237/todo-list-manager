060f800fd55d9e6d6c4fb29b5fe7cb253930e073
Robert <vvasquez@memphis.edu>
2026-07-05 14:13:03 -0500

    Initial import of To-Do List Manager

---
+++ todo_list.py
@@ -0,0 +1,117 @@
+import time, os, random
+
+todo = []
+fileExist = True  # Update 
+try:  # Update 
+  f = open('todo.list', 'r')  # Auto Load
+  todo = eval(f.read())  # Make string to working code
+  f.close()
+except:  
+  fileExist = False
+
+
+def add():
+  time.sleep(1)
+  os.system("clear")
+  task = input("Task: ").title()
+  due = input("Due by: ")
+  priority = input("Priority (low, medium, high): ").capitalize()
+
+  row = [task, due, priority]
+  todo.append(row)
+  print("Task Successfully added")
+
+
+def view():
+  time.sleep(1)
+  os.system('clear')
+  print("1: View All \n2: View Priority\n")
+  menu = input("> ")
+
+  if menu == '1':
+    for row in todo:
+      for item in row:
+        print(f"{item:^5}", end=" | ")
+      print()
+    print()
+  elif menu == '2':
+    priority = input('See low, medium, or high priority tasks: ').capitalize()
+    for row in todo:
+      if priority in row:
+        for item in row:
+          print(f"{item:^10}", end="")
+        print()
+      print()
+  else:
+    print("Make a valid selection")
+  time.sleep(1)
+
+
+def remove():
+  time.sleep(1)
+  os.system("clear")
+  name = input("Name of task to remove: ").lower()
+  for row in todo:
+    if name in row:
+      print(f"\n{name} has been deleted\n")
+      todo.remove(row)
+
+
+def edit():
+  time.sleep(1)
+  os.system("clear")
+  name = input("Name of task to edit: ").lower()
+
+  for row in todo:
+    if row[0] == name:
+      type = input("Edit: \n1: Name \n2: Date \n3: Priority\n")
+      if type == '1':
+        print(f"Change {row[0]} to: ")
+        row[0] = input("> ")
+      elif type == '2':
+        print(f"Change {row[1]} to: ")
+        row[1] = input("> ")
+      elif type == '3':
+        print(f"Change {row[2]} to: ")
+        row[2] = input("> ")
+      print("Task updated successfully")
+      break
+  else:
+    print("Task Not Found")
+
+
+while True:
+  print("TODO List Management System\n")
+  print("1: Add \n2: View \n3: Remove \n4: Edit \n5: Exit \n")
+
+  menu = input("> ")
+
+  if menu == '1':
+    add()
+  elif menu == '2':
+    view()
+  elif menu == '3':
+    remove()
+  elif menu == '4':
+    edit()
+  elif menu == '5':
+    exit()
+  else:
+    print("Make a valid selection")
+    continue
+
+  time.sleep(3)
+  os.system("clear")
+
+  # Update create backup folder
+  if fileExist:
+    try:
+      os.mkdir("Backups")
+    except:
+      pass
+    name = f"backup{random.randint(1,10000000000)}.txt"
+    os.popen(f"cp todo.list Backups/{name}")
+  
+  f = open('todo.list', 'w')
+  f.write(str(todo))
+  f.close()
